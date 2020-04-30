51b5ec46524db9f68dc59846f6acdacf11636292
BandhaviC <bandhavic6@gmail.com>
2020-04-30 16:30:43 -0700

    add data models

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core'
-import { ProductRepository } from './product.repository'
-import { StaticDataSource } from './static.datasource'
-import { Cart } from './cart.model'
-import { Order } from './order.model'
-import { OrderRepository } from './order.repository'
-import { HttpClientModule } from '@angular/common/http'
-import { RestDataSource } from './rest.datasource'
+import { NgModule } from "@angular/core";
+import { ProductRepository } from "./product.repository";
+import { StaticDataSource } from "./static.datasource";
+import { Cart } from "./cart.model";
+import { Order } from "./order.model";
+import { OrderRepository } from "./order.repository";
+import { HttpClientModule } from "@angular/common/http";
+import { RestDataSource } from "./rest.datasource";
+import { AuthService } from "./auth.service";
 
 // make its classes/services available to other modules
 @NgModule({
@@ -17,6 +18,7 @@ import { RestDataSource } from './rest.datasource'
     Cart,
     Order,
     OrderRepository,
+    AuthService,
   ],
 })
 export class ModelModule {}
